ed6e65c6c006a077e2ebee6d6e3d16e21c711736
Jianan Zhou <dr_neutron@126.com>
2021-11-30 17:03:02 +0800

    2021-11-30

--- Section4-JavaScript/Chapter03-JQuery/第2天/09-电梯导航/js/index.js
+++ Section4-JavaScript/Chapter03-JQuery/第2天/09-电梯导航/js/index.js
@@ -17,21 +17,16 @@ $(function() {
     $(window).scroll(function() {
         toggleTool();
         // 3. 页面滚动到某个内容区域，左侧电梯导航小li相应添加和删除current类名
-
-
         if (flag) {
             $(".floor .w").each(function(i, ele) {
                 if ($(document).scrollTop() >= $(ele).offset().top) {
                     console.log(i);
                     $(".fixedtool li").eq(i).addClass("current").siblings().removeClass();
-
                 }
             })
         }
-
-
-
     });
+
     // 2. 点击电梯导航页面可以滚动到相应内容区域
     $(".fixedtool li").click(function() {
         flag = false;

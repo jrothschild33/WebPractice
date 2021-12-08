2f5bc2ea461a2dfd16fde2f27fa7746c793e3e5b
jrothschild33 <dr_neutron@126.com>
2021-12-08 17:00:13 +0800

    2021-12-08

--- Section4-JavaScript/Chapter05-JavaScript高级/第4天/代码/08-表单验证/js/reg.js
+++ Section4-JavaScript/Chapter05-JavaScript高级/第4天/代码/08-表单验证/js/reg.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+window.onload = function () {
     var regtel = /^1[3|4|5|7|8]\d{9}$/; // 手机号码的正则表达式
     var regqq = /^[1-9]\d{4,}$/; // 10000
     var regnc = /^[\u4e00-\u9fa5]{2,8}$/;
@@ -10,35 +10,39 @@ window.onload = function() {
     var msg = document.querySelector('#msg');
     var pwd = document.querySelector('#pwd');
     var surepwd = document.querySelector('#surepwd');
+
     regexp(tel, regtel); // 手机号码
     regexp(qq, regqq); // qq号码
     regexp(nc, regnc); // 昵称
     regexp(msg, regmsg); // 短信验证
     regexp(pwd, regpwd); // 密码框
+
     // 表单验证的函数
     function regexp(ele, reg) {
-        ele.onblur = function() {
+        ele.onblur = function () {
             if (reg.test(this.value)) {
                 // console.log('正确的');
                 this.nextElementSibling.className = 'success';
-                this.nextElementSibling.innerHTML = '<i class="success_icon"></i> 恭喜您输入正确';
+                this.nextElementSibling.innerHTML =
+                    '<i class="success_icon"></i> 恭喜您输入正确';
             } else {
                 // console.log('不正确');
                 this.nextElementSibling.className = 'error';
-                this.nextElementSibling.innerHTML = '<i class="error_icon"></i> 格式不正确，请从新输入 ';
+                this.nextElementSibling.innerHTML =
+                    '<i class="error_icon"></i> 格式不正确，请从新输入 ';
             }
-        }
-    };
-
-    surepwd.onblur = function() {
-        if (this.value == pwd.value) {
-            this.nextElementSibling.className = 'success';
-            this.nextElementSibling.innerHTML = '<i class="success_icon"></i> 恭喜您输入正确';
-        } else {
-            this.nextElementSibling.className = 'error';
-            this.nextElementSibling.innerHTML = '<i class="error_icon"></i> 两次密码输入不一致';
-
-        }
+        };
     }
 
-}
+    surepwd.onblur = function () {
+        if (this.value == pwd.value) {
+            this.nextElementSibling.className = 'success';
+            this.nextElementSibling.innerHTML =
+                '<i class="success_icon"></i> 恭喜您输入正确';
+        } else {
+            this.nextElementSibling.className = 'error';
+            this.nextElementSibling.innerHTML =
+                '<i class="error_icon"></i> 两次密码输入不一致';
+        }
+    };
+};

3ccfef9606b37f504fe108434039bee6e3dd721d
Jason Zhou <dr_neutron@126.com>
2021-12-20 23:20:23 +0800

    2021-12-20

--- Section5-Server/Chapter03-项目实战/第3天/代码/assets/js/index.js
+++ Section5-Server/Chapter03-项目实战/第3天/代码/assets/js/index.js
@@ -1,28 +1,24 @@
 $(function () {
     // 调用 getUserInfo 获取用户基本信息
-    getUserInfo();
+    getUserInfo()
 
-    var layer = layui.layer;
+    var layer = layui.layer
 
     // 点击按钮，实现退出功能
     $('#btnLogout').on('click', function () {
         // 提示用户是否确认退出
-        layer.confirm(
-            '确定退出登录?',
-            { icon: 3, title: '提示' },
-            function (index) {
-                //do something
-                // 1. 清空本地存储中的 token
-                localStorage.removeItem('token');
-                // 2. 重新跳转到登录页面
-                location.href = '/login.html';
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
+            //do something
+            // 1. 清空本地存储中的 token
+            localStorage.removeItem('token')
+            // 2. 重新跳转到登录页面
+            location.href = '/login.html'
 
-                // 关闭 confirm 询问框
-                layer.close(index);
-            }
-        );
-    });
-});
+            // 关闭 confirm 询问框
+            layer.close(index)
+        })
+    })
+})
 
 // 获取用户的基本信息
 function getUserInfo() {
@@ -31,10 +27,10 @@ function getUserInfo() {
         url: '/my/userinfo',
         success: function (res) {
             if (res.status !== 0) {
-                return layui.layer.msg('获取用户信息失败！');
+                return layui.layer.msg('获取用户信息失败！')
             }
             // 调用 renderAvatar 渲染用户的头像
-            renderAvatar(res.data);
+            renderAvatar(res.data)
         },
         // 不论成功还是失败，最终都会调用 complete 回调函数
         // complete: function(res) {
@@ -48,24 +44,24 @@ function getUserInfo() {
         //     location.href = '/login.html'
         //   }
         // }
-    });
+    })
 }
 
 // 渲染用户的头像
 function renderAvatar(user) {
     // 1. 获取用户的名称
-    var name = user.nickname || user.username;
+    var name = user.nickname || user.username
     // 2. 设置欢迎的文本
-    $('#welcome').html('欢迎&nbsp;&nbsp;' + name);
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
     // 3. 按需渲染用户的头像
     if (user.user_pic !== null) {
         // 3.1 渲染图片头像
-        $('.layui-nav-img').attr('src', user.user_pic).show();
-        $('.text-avatar').hide();
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()
     } else {
         // 3.2 渲染文本头像
-        $('.layui-nav-img').hide();
-        var first = name[0].toUpperCase();
-        $('.text-avatar').html(first).show();
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
     }
 }

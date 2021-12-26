3b7ee1df86f30fd51df8953506de916153244132
Jason Zhou <dr_neutron@126.com>
2021-12-26 23:16:12 +0800

    2021-12-26

--- Section5-Server/Chapter04-Node.js/第1天/代码/03.写入文件内容.js
+++ Section5-Server/Chapter04-Node.js/第1天/代码/03.写入文件内容.js
@@ -5,13 +5,13 @@ const fs = require('fs')
 //    参数1：表示文件的存放路径
 //    参数2：表示要写入的内容
 //    参数3：回调函数
-fs.writeFile('./files/3.txt', 'ok123', function(err) {
-  // 2.1 如果文件写入成功，则 err 的值等于 null
-  // 2.2 如果文件写入失败，则 err 的值等于一个 错误对象
-  // console.log(err)
+fs.writeFile('./files/3.txt', 'ok123', function (err) {
+    // 2.1 如果文件写入成功，则 err 的值等于 null
+    // 2.2 如果文件写入失败，则 err 的值等于一个 错误对象
+    // console.log(err)
 
-  if (err) {
-    return console.log('文件写入失败！' + err.message)
-  }
-  console.log('文件写入成功！')
+    if (err) {
+        return console.log('文件写入失败！' + err.message)
+    }
+    console.log('文件写入成功！')
 })

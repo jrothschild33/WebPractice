4b9fa80807f878e9347ce3448ef2aee6235efe26
jrothschild33 <dr_neutron@126.com>
2021-12-27 17:12:12 +0800

    2021-12-27

--- Section5-Server/Chapter04-Node.js/第2天/代码/08.模块作用域.js
+++ Section5-Server/Chapter04-Node.js/第2天/代码/08.模块作用域.js
@@ -1,5 +1,6 @@
 const username = '张三'
 
 function sayHello() {
-  console.log('大家好，我是' + username)
+    console.log('大家好，我是' + username)
 }
+console.log(module)
--- Section5-Server/Chapter04-Node.js/第2天/代码/11.自定义模块.js
+++ Section5-Server/Chapter04-Node.js/第2天/代码/11.自定义模块.js
@@ -5,15 +5,15 @@ const age = 20
 // 向 module.exports 对象上挂载 username 属性
 module.exports.username = 'zs'
 // 向 module.exports 对象上挂载 sayHello 方法
-module.exports.sayHello = function() {
-  console.log('Hello!')
+module.exports.sayHello = function () {
+    console.log('Hello!')
 }
 module.exports.age = age
 
 // 让 module.exports 指向一个全新的对象
 module.exports = {
-  nickname: '小黑',
-  sayHi() {
-    console.log('Hi!')
-  }
+    nickname: '小黑',
+    sayHi() {
+        console.log('Hi!')
+    },
 }

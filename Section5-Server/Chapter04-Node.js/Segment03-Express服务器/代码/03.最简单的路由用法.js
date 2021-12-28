8a7e00d7aeb15f8a815620b9457a72843d313f5a
jrothschild33 <dr_neutron@126.com>
2021-12-28 16:50:08 +0800

    2021-12-28

--- Section5-Server/Chapter04-Node.js/Segment03-Express服务器/代码/03.最简单的路由用法.js
+++ Section5-Server/Chapter04-Node.js/Segment03-Express服务器/代码/03.最简单的路由用法.js
@@ -3,12 +3,12 @@ const app = express()
 
 // 挂载路由
 app.get('/', (req, res) => {
-  res.send('hello world.')
+    res.send('hello world.')
 })
 app.post('/', (req, res) => {
-  res.send('Post Request.')
+    res.send('Post Request.')
 })
 
 app.listen(80, () => {
-  console.log('http://127.0.0.1')
+    console.log('http://127.0.0.1')
 })
--- Section5-Server/Chapter04-Node.js/Segment03-Express服务器/代码/05.模块化路由.js
+++ Section5-Server/Chapter04-Node.js/Segment03-Express服务器/代码/05.模块化路由.js
@@ -4,12 +4,12 @@ const app = express()
 // app.use('/files', express.static('./files'))
 
 // 1. 导入路由模块
-const router = require('./03.router')
+const router = require('./04.router')
 // 2. 注册路由模块
 app.use('/api', router)
 
 // 注意： app.use() 函数的作用，就是来注册全局中间件
 
 app.listen(80, () => {
-  console.log('http://127.0.0.1')
+    console.log('http://127.0.0.1')
 })

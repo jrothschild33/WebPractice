bcd2b183149cc960161171d3da8a25ae326d88a5
Jason Zhou <dr_neutron@126.com>
2022-02-16 23:43:40 +0800

    2022-02-16

--- Web_Basic/Section5-Server/Chapter03-Node.js/Segment03-Express服务器/代码/15.对自定义的中间件进行模块化的拆分.js
+++ Web_Basic/Section5-Server/Chapter03-Node.js/Segment03-Express服务器/代码/15.对自定义的中间件进行模块化的拆分.js
@@ -4,15 +4,15 @@ const express = require('express')
 const app = express()
 
 // 1. 导入自己封装的中间件模块
-const customBodyParser = require('./14.custom-body-parser')
+const customBodyParser = require('./16.custom-body-parser')
 // 2. 将自定义的中间件函数，注册为全局可用的中间件
 app.use(customBodyParser)
 
 app.post('/user', (req, res) => {
-    res.send(req.body)
+  res.send(req.body)
 })
 
 // 调用 app.listen 方法，指定端口号并启动web服务器
 app.listen(80, function () {
-    console.log('Express server running at http://127.0.0.1')
+  console.log('Express server running at http://127.0.0.1')
 })
